3004d24bf728be47ef67031aea60453f83f0093d
Blerton Ismaili <ismaili.blerton.03@gmail.com>
2024-01-31 16:25:18 +0100

    Task 22
    Write a C++ program to find sum of upper triangular matrix.

---
+++ Task 22/Task 22.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Write a C++ program to find sum of upper triangular matrix.
+
+int main()
+{
+	const int size = 3;
+	int arr[size][size] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+	int sum = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (j > i) {
+				sum += arr[i][j];
+			}
+		}
+	}
+
+	cout << "The sum of the upper triangular matrix is: " << sum << endl;
+	return 0;
+}
