7f5de62d1c27852c32d9a68b8912fd72e0071e2b
Blerton Ismaili <ismaili.blerton.03@gmail.com>
2024-01-31 16:07:02 +0100

    Task 19
    Write a C++ program to interchange diagonals of a matrix.

---
+++ Task 19/Task 19.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// Write a C++ program to interchange diagonals of a matrix.
+
+int main()
+{
+	const int size = 3;
+	int arr[size][size] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (i == j) {
+				int temp = arr[size - 1 - i][j];
+				arr[size - 1 - i][j] = arr[i][j];
+				arr[i][j] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
