1c752c9337d8011c73b47cf015fb2a2465b60884
Blerton Ismaili <ismaili.blerton.03@gmail.com>
2024-01-31 16:19:54 +0100

    Task 21
    Write a C++ program to find lower triangular matrix.

---
+++ Task 21/Task 21.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Write a C++ program to find lower triangular matrix.
+
+int main()
+{
+	const int size = 3;
+	int arr[size][size] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (i > j) {
+				cout << arr[i][j] << " ";
+			}
+		}
+	}
+	return 0;
+}
